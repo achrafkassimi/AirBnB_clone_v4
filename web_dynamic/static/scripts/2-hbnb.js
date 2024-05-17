d907c0c3a3a8745e1309923dcb9092561a82c016
achrafkassimi <achraf.kassimi.1995@gmail.com>
2024-05-17 01:43:05 +0100

    port 5003

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', function () {
   // task 3
-  $.ajax('http://0.0.0.0:5001/api/v1/status').done(function (data) {
+  $.ajax('http://0.0.0.0:5003/api/v1/status').done(function (data) {
     if (data.status === 'OK') {
       $('#api_status').addClass('available');
     } else {
